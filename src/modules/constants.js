46120311c57bb25c1566b34ccc3ec5697e4eb1fe
belaymit <2belamit@gmail.com>
2022-12-20 08:50:12 +0300

    declared a variable to store close icon

--- src/modules/constants.js
+++ src/modules/constants.js
@@ -1,4 +1,4 @@
-export const searchBtn = document.getElementById('.search-btn');
+export const searchBtn = document.getElementById('search-btn');
 export const mealList = document.getElementById('meal');
-export const mealDetailsContent = document.querySelector('.meals-detail-content');
+export const mealDetailsContent = document.querySelector('.meal-details-content');
 export const recipeCloseBtn = document.getElementById('recipe-close-btn');
